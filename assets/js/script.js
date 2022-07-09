a133d114bf2a523d88a4f0616805872bdd209ed2
George Murphy <george.moguls@gmail.com>
2022-07-09 21:15:08 +1000

    fixed 5 day forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,8 +7,6 @@ var key = "d332c011d69ce2ab0548271b8a1515d6";
 var fiveDayForecast = $('.5day-forecast');
 var previousCityContainer = $('.city-list');
 
-getWeatherToday();
-
 function getWeatherToday() {
     var requestURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${key}`;
     fetch(requestURL)
@@ -45,13 +43,15 @@ function displayWeather(data) {
 
 function uvIndexDisplay(data) {
     $('#UV').text('UV Index: ');
-    var uvIndexSpan = $('<span>').text(data.current.uvi);
+    var uvIndexSpan = $('<span>')
+    uvIndexSpan.text(data.current.uvi);
     $('#UV').append(uvIndexSpan);
-    if (uvIndexSpan >= 0 && uvIndexSpan <= 3) {
+    console.log(data.current.uvi);
+    if (data.current.uvi >= 0 && data.current.uvi <= 3) {
         uvIndexSpan.attr('class', 'favourable');
-    } else if (uvIndexSpan > 3 && uvIndexSpan <= 6) {
+    } else if (data.current.uvi > 3 && data.current.uvi <= 6) {
         uvIndexSpan.attr('class', 'moderate');
-    } else (uvIndexSpan > 6 && uvIndexSpan <= 9) 
+    } else (data.current.uvi > 6 && data.current.uvi <= 9) 
         uvIndexSpan.attr('class', 'severe');
     
     getFiveDayForecast();
@@ -64,7 +64,6 @@ function getFiveDayForecast() {
         .then(function(response) {
             if (response.ok) {
                 response.json().then(function(data) {
-                    console.log(data);
                     displayFiveDayForecast(data);
                 })
             }
@@ -72,21 +71,21 @@ function getFiveDayForecast() {
 }
 
 function displayFiveDayForecast(data) {
-    var dayOne = data.list[1];
-    var dayTwo = data.list[2];
-    var dayThree = data.list[3];
-    var dayFour = data.list[4];
-    var dayFive = data.list[5];
+    var dayOne = data.list[0];
+    var dayTwo = data.list[7];
+    var dayThree = data.list[14];
+    var dayFour = data.list[21];
+    var dayFive = data.list[28];
     var fiveDayArray = [dayOne, dayTwo, dayThree, dayFour, dayFive];
-    console.log(fiveDayArray);
     fiveDayArray.forEach(function(forecast){
         var fiveDayDiv = $('<div>');
-        fiveDayDiv.attr('class', 'col-md-2 card')
+        fiveDayDiv.attr('class', 'col-md-3 card')
         fiveDayForecast.append(fiveDayDiv);
 
         var fiveDayDivHeader = $('<h1>');
         fiveDayDivHeader.attr('class', 'five-day-header');
-        fiveDayDivHeader.text(forecast.dt_txt);
+        date = forecast.dt_txt.split(' ')[0]
+        fiveDayDivHeader.text(moment(date).format('DD-MM-YYYY'));
         fiveDayDiv.append(fiveDayDivHeader);
 
         var fiveDayDivBody = $('<div>');
@@ -94,7 +93,7 @@ function displayFiveDayForecast(data) {
         fiveDayDiv.append(fiveDayDivBody);
 
         var fiveDayDivIcon = $('<img>');
-        fiveDayDivIcon.attr('src', `https://openweathermap.org/img/wn/${forecast.weather[i].icon}@2x.png`);
+        fiveDayDivIcon.attr('src', `https://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png`);
         fiveDayDivIcon.attr('class', 'weather-icon');
         fiveDayDivBody.append(fiveDayDivIcon);
 
@@ -108,37 +107,6 @@ function displayFiveDayForecast(data) {
         var humidityText = $('<p>').text('Humidity: ' + forecast.main.humidity + ' %');
         fiveDayDivBody.append(humidityText);
     });
-    /*
-    for (var i=0; i < fiveDayArray.length; i++) {
-        var fiveDayDiv = $('<div>');
-        fiveDayDiv.attr('class', 'col-md-2 card')
-        fiveDayForecast.append(fiveDayDiv);
-
-        var fiveDayDivHeader = $('<h1>');
-        fiveDayDivHeader.attr('class', 'five-day-header');
-        fiveDayDivHeader.text(fiveDayArray[i].dt_txt);
-        fiveDayDiv.append(fiveDayDivHeader);
-
-        var fiveDayDivBody = $('<div>');
-        fiveDayDivBody.attr('class', 'five-day-body');
-        fiveDayDiv.append(fiveDayDivBody);
-
-        var fiveDayDivIcon = $('<img>');
-        fiveDayDivIcon.attr('src', `https://openweathermap.org/img/wn/${fiveDayArray[i].weather[i].icon}@2x.png`);
-        fiveDayDivIcon.attr('class', 'weather-icon');
-        fiveDayDivBody.append(fiveDayDivIcon);
-
-        console.log(fiveDayArray);
-        var tempText = $('<p>').text('Temperature: ' + fiveDayArray[i].main.temp + '°C');
-        fiveDayDivBody.append(tempText);
-
-        var windText = $('<p>').text('Wind: ' + fiveDayArray[i].wind.speed + ' KPH');
-        fiveDayDivBody.append(windText);
-
-        var humidityText = $('<p>').text('Humidity: ' + fiveDayArray[i].main.humidity + ' %');
-        fiveDayDivBody.append(humidityText);
-    }
-    */
 }
 
 previousCitySearch = [];
@@ -176,6 +144,7 @@ function preload() {
         previousCitySearch = cityStorage
     }
     previousCities();
+    getWeatherToday();
 }
 
 preload();
